3190f80c14a6c500d845677263ac99cba2da5d9a
aaronJau21 <aaronjau21@gmail.com>
2023-10-29 16:23:57 -0500

    migrations and Enums

---
+++ adminTask-backend/app/Enums/PriorityEnums.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum PriorityEnum: int
+{
+    case high = 3;
+    case intermediate = 2;
+    case normal = 1;
+}
---
+++ adminTask-backend/app/Enums/RoleEnums.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+
+enum RoleEnums: string
+{
+    case User = "User";
+    case Administrador = "Administrador";
+}
